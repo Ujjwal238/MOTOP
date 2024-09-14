7c4da86726ec3ec98044761f96c15da79b4d578d
Ujjwal238 <ujjwalmishra238@gmail.com>
2024-09-14 11:15:53 +0530

    changes

--- model.py
+++ model.py
@@ -12,33 +12,41 @@ import math
 #%%
 #this is the multi headed attention layer
 # the naming is same as target
-#  
+#  i made this fourth
+# 1024 tokens of dim 728 get in it 
 
 class CasualSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
-        assert config.n_embd % config.n_head == 0
+        assert config.n_embd % config.n_head == 0 # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(config.n_embd , 3 * config.n_embd )
+        # output projection
         self.c_proj = nn.Linear(config.n_embd , config.n_embd)
+        # regularization  
         self.n_head = config.n_head
         self.n_embd = config.n_embd
 
         self.register_buffer("bias",torch.tril(torch.ones(config.block_size,config.block_size)).view(1,1,config.block_size,config.block_size))
     
     def forward(self,x):
-        B,T,C = x.size()
+        B,T,C = x.size()# batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=C=768 channels in the Transformer
 
-        qkv =self.c_attn(x)
+        qkv =self.c_attn(x) #qkv multiplication
         q,k,v =qkv.split(self.n_embd,dim=2)
-        k = k.view(B,T,self.n_head, C // self.n_head).transpose(1,2)
-        q = q.view(B,T,self.n_head, C // self.n_head).transpose(1,2)
-        v  = v.view(B,T,self.n_head, C // self.n_head).transpose(1,2)
+        k = k.view(B,T,self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, hs)
+        q = q.view(B,T,self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, hs)
+        v  = v.view(B,T,self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, hs)
 
         att =(q @ k.transpose (-2,-1)) * (1.0/ math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0 ,float('-inf'))
-        att =F.softmax(att,dim =-1)
-        y= att @ v #matt mul
-        y = y.transpose(1,2).contiguous().view(B,T,C)
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0 ,float('-inf')) #autoregressive mask
+        att =F.softmax(att,dim =-1) #normalize
+        y= att @ v #matt mul (B,nh,T,T) x (B,nh,T,hs) -> (B,nh,T,hs) #weighted sum
+
+        y = y.transpose(1,2).contiguous().view(B,T,C)# re-assemble all head outputs side by side
+        # output projection
         y = self.c_proj(y)
         return y
 
@@ -123,18 +131,21 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd,config.vocab_size,bias =False)
-
+#this was made to send data to my model
     def forward (self,idx):
         B,T = idx.size()
+        # idx is of shape (B, T) block size with t tokens in a sequence b staked up
         assert T <= self.config.block_size, f"cant forward seq of length {T}, block size is only {self.config.block_size}"
-        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
-        pos_emb = self.transformer.wpe(pos)
-        tok_emb = self.transformer.wte(idx)
-        x = tok_emb + pos_emb
+         # forward the token and posisition embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device) # shape (T)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (T, n_embd) 
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (B, T, n_embd)
+        x = tok_emb + pos_emb  # forward the blocks of the transformer
         for block in self.transformer.h:
-            x = block(x)
+            x = block(x) 
+        # forward the final layernorm and the classifier
         x = self.transformer.ln_f(x)
-        logits = self.lm_head(x)
+        logits = self.lm_head(x) # (B, T, vocab_size)
         
         return logits
 
